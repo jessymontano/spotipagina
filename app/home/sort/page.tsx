4b324683004d00d9442de21222ad4c2a3cc4c218
jessica <jessymontano@hotmail.com>
2024-07-05 17:46:40 -0700

    agregar nombre e imagen de playlist en /sort

--- app/home/sort/page.tsx
+++ app/home/sort/page.tsx
@@ -1,18 +1,20 @@
 'use client';
 
-import { updatePlaylist } from "@/app/lib/spotify-sdk/utils";
+import { updatePlaylist} from "@/app/lib/spotify-sdk/utils";
 import { useSession } from "next-auth/react";
 import sdk from "@/app/lib/spotify-sdk/ClientInstance";
 import { useSearchParams } from "next/navigation";
-import { useState, useEffect } from "react";
-import { Track } from "@spotify/web-api-ts-sdk";
+import { useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Page() {
     const session = useSession();
     const [order, setOrder] = useState(true);
     const searchParams = useSearchParams();
     const playlistId = searchParams.get('playlistId') ?? '';
+    const playlistName = searchParams.get('name') ?? 'Sin nombre';
+    const playlistImage = searchParams.get('image') ?? '/images/temp.png';
     const [status, setStatus] = useState(0);
     const [ isSorting, setIsSorting] = useState(false);
     const [isSorted, setIsSorted] = useState(false);
@@ -33,17 +35,19 @@ export default function Page() {
         <div>
             {!isSorting && !isSorted && (
                 <div className="bg-black bg-opacity-70 max-w-3xl p-5 m-6 mx-auto">
-                    <div className="text-center">
-                        <h1 className="text-slate-100 text-2xl font-semibold m-4 p-2">Sortear pleilist</h1>
-                        {/**TODO: poner titulo e imagen de playlist */}
-                        <h2></h2>
-                    </div>
+                    <h1 className="text-2xl text-white font-semibold mt-4 p-2 col text-center">Sortear playlist:</h1>
+                    <div className="flex items-start">
+                        <div className="mx-6 mb-8 p-5">
+                            <h2 className="text-lg text-center mb-2 text-white m-2 p-2">{playlistName}</h2>
+                            <Image src={playlistImage} alt={playlistName} width={200} height={200}/>
+                        </div>
+                        <div className="mr-6 mb-8 p-5">
                     <label 
-                    className="mb-3 mt-5 block text-lg font-medium text-slate-100"
+                    className="mb-6 mt-5 block text-lg font-medium text-white"
                     htmlFor="order"
                     >Elige el orden:</label>
                     <select
-                    className="minecraft-btn text-md peer w-full bg-slate-500 py-[9px] pl-10 outline-2 placeholder:text-slate-300 mt-4 flex mx-auto justify-center text-white truncate p-1 border-2 border-b-4 hover:text-yellow-200" 
+                    className="minecraft-btn text-md peer w-80 pl-10  mb-8 outline-2 placeholder:text-slate-300 mt-4 flex mx-auto justify-center text-white truncate p-2 border-2 border-b-4 hover:text-yellow-200" 
                     name="order" 
                     id="order"
                     onChange={(e) => setOrder(e.target.value === "true")}
@@ -53,17 +57,18 @@ export default function Page() {
                     </select>
                     <div className="text-center">
                     <button 
-                    className="minecraft-btn mt-6 flex mx-auto w-64 justify-center text-white truncate p-1 border-2 border-b-4 hover:text-yellow-200"
+                    className="minecraft-btn mt-10 flex mx-auto w-64 justify-center text-white truncate p-1 border-2 border-b-4 hover:text-yellow-200"
                     onClick={sortPlaylist}
                     >
                         Sortear playlist
                     </button>
                     </div>
+                    </div>
+                    </div>
                 </div>
                 )
             }
             {isSorting && (
-                //TODO: agregar progress bar
                 <div className="text-center m-6 p-6 text-white justify-center bg-black bg-opacity-70">
                     <h1 className="p-4 mb-8 text-lg">Acomodando playlist: {status}%</h1>
                 <div className="overflow-hidden h-10 mb-10 text-xs flex bg-black border border-white ">
@@ -72,7 +77,6 @@ export default function Page() {
                 </div>
             )}
             {isSorted && (
-                //TODO: hacer mas bonito
                 <div className="text-center mx-auto bg-black bg-opacity-70 max-w-3xl p-8 m-6">
                     <h3 className="text-2xl text-slate-100 font-bold m-4">Playlist acomodada exitosamente! uwu</h3>
                     <p className="text-lg text-slate-100 font-semibold m-4">listo fakiu</p>
