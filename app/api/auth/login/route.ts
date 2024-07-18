71d439a402e7f20474f254be502e9b3015ff8c8c
jessica <jessymontano@hotmail.com>
2024-07-17 17:41:48 -0700

    por que no sirve el login

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -3,9 +3,6 @@ import { authenticate } from '@/app/lib/auth';
 
 //api para hacer login nose por que lo separe en otro archivo
 export async function POST(request: NextRequest) {
-    if (request.method !== 'POST'){
-        return NextResponse.json({error: 'Method not allowed'}, {status: 405});
-    }
     const body = await request.json();
     return authenticate(body);
 }
